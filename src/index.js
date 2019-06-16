2fe1ba4c151a0e9da9231d9679f1ed29471f7e75
Ananev Andrei <35199801+ananevandrei11@users.noreply.github.com>
2019-06-16 22:43:38 +0400

    starting form&elements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.less';
 import 'jquery';
 import _ from 'lodash';
 import './components/form&elements/datepicker.min.js';
+import './components/form&elements/bootstrap-input-spinner.js';
 
   function component() {}
 
