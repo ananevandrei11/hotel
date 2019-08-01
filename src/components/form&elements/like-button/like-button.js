24f5587d99a4072fd44c4eb2ee5544989d124c1f
Ananev Andrei <35199801+ananevandrei11@users.noreply.github.com>
2019-08-01 22:07:42 +0400

    refact form elements

--- src/components/form&elements/like-button/like-button.js
+++ src/components/form&elements/like-button/like-button.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
     $(".like-button").one('click', function () {
-        if ($("#favorite").attr('value') == "0") {
-            $("#favorite").attr('value', '0');
-        } else {
             $("#favorite").attr('value', parseInt($("#favorite").val()) + 1);
-            $(".like-button").css("border-color", "#BC9CFF");
+            $("#favorite").attr("readonly");
             $(".like-button > .material-icons").html("favorite");
             $(".like-button > .material-icons").css("color", "#BC9CFF");
             $("#favorite").css("color", "#BC9CFF");
-        }
     });
 });
