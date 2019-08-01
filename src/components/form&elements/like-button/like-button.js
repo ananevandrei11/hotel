4da7fe6fe19ad067a5e9f5abca1fe422e689fdcf
Ananev Andrei <35199801+ananevandrei11@users.noreply.github.com>
2019-08-02 01:03:07 +0400

    refact second today

--- src/components/form&elements/like-button/like-button.js
+++ src/components/form&elements/like-button/like-button.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
-    $(".like-button").one('click', function () {
+    $("#like-button").click(function () {
+          if ($(".like-button").hasClass("like-button-border")) {
             $("#favorite").attr('value', parseInt($("#favorite").val()) + 1);
-            $("#favorite").attr("readonly");
+            $("#like-button").removeClass("like-button-border").addClass("like-button-border-checked");
             $(".like-button > .material-icons").html("favorite");
             $(".like-button > .material-icons").css("color", "#BC9CFF");
             $("#favorite").css("color", "#BC9CFF");
-    });
+          } else {
+            $("#favorite").attr('value', parseInt($("#favorite").val()) - 1);
+            $("#like-button").removeClass("like-button-border-checked").addClass("like-button-border");
+            $(".like-button > .material-icons").html("favorite_border");
+            $(".like-button > .material-icons").css("color", "rgba(31, 32, 65, 0.25)");
+            $("#favorite").css("color", "rgba(31, 32, 65, 0.25)");
+          }
+       
+    });     
 });
