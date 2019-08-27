9373b7bdb3b7d3cae2530fea56c6ff941a2f78c7
Ananev Andrei <35199801+ananevandrei11@users.noreply.github.com>
2019-08-27 22:03:44 +0400

    refac form elements

--- src/components/form&elements/rate-button/rate-button.js
+++ src/components/form&elements/rate-button/rate-button.js
@@ -1,47 +1,66 @@
 $(document).ready(function () {
-  $("#one-star").click(function () {
-    if ($("#one-star").hasClass("star-border")) {
-      $("#one-star").html("star");
-      $("#one-star").removeClass("star-border").addClass("star");
-    } else {
-      $("#one-star, #two-star, #three-star, #four-star, #five-star").html("star_border");
-      $("#one-star, #two-star, #three-star, #four-star, #five-star").removeClass("star").addClass("star-border");
-    }
-  });
-  $("#two-star").click(function () {
-    if ($("#two-star").hasClass("star-border")) {
-      $("#one-star, #two-star").html("star");
-      $("#one-star, #two-star").removeClass("star-border").addClass("star");
-    } else {
-      $("#two-star, #three-star, #four-star, #five-star").html("star_border");
-      $("#two-star, #three-star, #four-star, #five-star").removeClass("star").addClass("star-border");
-    }
-  });
-  $("#three-star").click(function () {
-    if ($("#three-star").hasClass("star-border")) {
-      $("#one-star, #two-star, #three-star").html("star");
-      $("#one-star, #two-star, #three-star").removeClass("star-border").addClass("star");
-    } else {
-      $("#three-star, #four-star, #five-star").html("star_border");
-      $("#three-star, #four-star, #five-star").removeClass("star").addClass("star-border");
-    }
-  });
-  $("#four-star").click(function () {
-    if ($("#four-star").hasClass("star-border")) {
-      $("#one-star, #two-star, #three-star, #four-star").html("star");
-      $("#one-star, #two-star, #three-star, #four-star").removeClass("star-border").addClass("star");
-    } else {
-      $("#four-star, #five-star").html("star_border");
-      $("#four-star, #five-star").removeClass("star").addClass("star-border");
-    }
-  });
-  $("#five-star").click(function () {
-    if ($("#five-star").hasClass("star-border")) {
-      $("#one-star, #two-star, #three-star, #four-star, #five-star").html("star");
-      $("#one-star, #two-star, #three-star, #four-star, #five-star").removeClass("star-border").addClass("star");
-    } else {
-      $("#five-star").html("star_border");
-      $("#five-star").removeClass("star").addClass("star-border");
-    }
-  });
-});
+  /*STAR ONE*/
+$("#label_rating_1").click(function () {
+      if ($("#label_rating_1").hasClass("star-border")) {
+        $("#label_rating_1").html("star");
+        $("#rating_1").attr("checked", "checked");
+        $("#label_rating_1").removeClass("star-border").addClass("star");
+      } else {
+        $("#label_rating_1, #label_rating_2, #label_rating_3, #label_rating_4, #label_rating_5").html("star_border");
+        $("#label_rating_1, #label_rating_2, #label_rating_3, #label_rating_4, #label_rating_5").removeClass("star").addClass("star-border");
+        $("#rating_1, #rating_2, #rating_3, #rating_4, #rating_5").removeAttr("checked", "checked");
+      }      
+    });
+ 
+    /*STAR TWO*/
+$("#label_rating_2").click(function () {
+      if ($("#label_rating_2").hasClass("star-border")) {
+        $("#label_rating_1, #label_rating_2").html("star");
+        $("#rating_1, #rating_2").attr("checked", "checked");
+        $("#label_rating_1, #label_rating_2").removeClass("star-border").addClass("star");
+      } else {
+        $("#label_rating_2, #label_rating_3, #label_rating_4, #label_rating_5").html("star_border");
+        $("#label_rating_2, #label_rating_3, #label_rating_4, #label_rating_5").removeClass("star").addClass("star-border");
+        $("#rating_2, #rating_3, #rating_4, #rating_5").removeAttr("checked", "checked");
+      }      
+    });
+  
+      /*STAR THREE*/
+$("#label_rating_3").click(function () {
+      if ($("#label_rating_3").hasClass("star-border")) {
+        $("#label_rating_1, #label_rating_2, #label_rating_3").html("star");
+        $("#rating_1, #rating_2, #rating_3").attr("checked", "checked");
+        $("#label_rating_1, #label_rating_2, #label_rating_3").removeClass("star-border").addClass("star");
+      } else {
+        $("#label_rating_3, #label_rating_4, #label_rating_5").html("star_border");
+        $("#label_rating_3, #label_rating_4, #label_rating_5").removeClass("star").addClass("star-border");
+        $("#rating_3, #rating_4, #rating_5").removeAttr("checked", "checked");
+      }      
+    });
+  
+       /*STAR FOUR*/
+$("#label_rating_4").click(function () {
+      if ($("#label_rating_4").hasClass("star-border")) {
+        $("#label_rating_1, #label_rating_2, #label_rating_3, #label_rating_4").html("star");
+        $("#rating_1, #rating_2, #rating_3, #rating_4").attr("checked", "checked");
+        $("#label_rating_1, #label_rating_2, #label_rating_3, #label_rating_4").removeClass("star-border").addClass("star");
+      } else {
+        $("#label_rating_4, #label_rating_5").html("star_border");
+        $("#label_rating_4, #label_rating_5").removeClass("star").addClass("star-border");
+        $("#rating_4, #rating_5").removeAttr("checked", "checked");
+      }      
+    });
+  
+       /*STAR FIVE*/
+$("#label_rating_5").click(function () {
+      if ($("#label_rating_5").hasClass("star-border")) {
+        $("#label_rating_1, #label_rating_2, #label_rating_3, #label_rating_4, #label_rating_5").html("star");
+        $("#rating_1, #rating_2, #rating_3, #rating_4, #rating_5").attr("checked", "checked");
+        $("#label_rating_1, #label_rating_2, #label_rating_3, #label_rating_4, #label_rating_5").removeClass("star-border").addClass("star");
+      } else {
+        $("#label_rating_5").html("star_border");
+        $("#label_rating_5").removeClass("star").addClass("star-border");
+        $("#rating_5").removeAttr("checked", "checked");
+      }      
+    });  
+ });
