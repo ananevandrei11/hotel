6253764feed61ae6f83e16d991e944e695f01515
ananevandrei11 <ananevandrei11@gmail.com>
2019-09-18 01:27:10 +0300

    create page cards

--- src/components/form&elements/date-dropdown/date-dropdown.js
+++ src/components/form&elements/date-dropdown/date-dropdown.js
@@ -1,19 +1,37 @@
-$(function() {
-  $('#date-start').datepicker({
-    range: true,
-    toggleSelected: false,
-    dateFormat: "dd.mm.yyyy",
-    clearButton: true
+$(function () {
+  var firstDatepicker = $('#date-start').datepicker({
+      autoClose: true,
+      minDate: new Date(),
+      onSelect: function(formattedDate, date, inst) {
+        // update secondDatepicker option, based on currently selected date
+        secondDatepicker.update({
+          minDate: new Date(date)
+        });
+        // clear secondDatepicker, because we have new minDate option
+        secondDatepicker.clear();
+      }
+    }).data('datepicker');
+    
+    var secondDatepicker = $('#date-end').datepicker({
+      autoClose: true,
+      minDate: new Date()
+    }).data('datepicker');
+    
+    $("#btn-start").click(function () {
+      if ($("#date-start").attr("disabled")) {
+        $("#date-start").removeAttr("disabled");
+        $("#date-start").select();
+      } else {
+        $("#date-start").attr("disabled", "disabled");
+      };
+    });
+  
+    $("#btn-end").click(function () {
+      if ($("#date-end").attr("disabled")) {
+        $("#date-end").removeAttr("disabled");
+        $("#date-end").select();
+      } else {
+        $("#date-end").attr("disabled", "disabled");
+      };
+    });
   });
-
-$("#btn-start, #btn-end").click(function () {
-  if ($("#date-start").attr("disabled")) {
-      $("#date-start").removeAttr("disabled");
-      $("#date-start").select();
-  } else {
-      $("#date-start").attr("disabled", "disabled");
-  }
-
-});
-});
-
